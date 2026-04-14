2c98dcb72e38afa079d109b8a89c92f2dae79fa1
Xinoi <91490471+Xinoi@users.noreply.github.com>
2026-04-14 14:59:54 +0200

    Update markdown.lua

--- nvim/plugin/markdown.lua
+++ nvim/plugin/markdown.lua
@@ -1,6 +1,15 @@
 require('render-markdown').setup({
     enabled = true,
     file_types = { 'markdown' },
+    latex = {
+        enabled = true,
+        render_modes = false,
+        converter = { 'utftex', 'latex2text' },
+        highlight = 'RenderMarkdownMath',
+        position = 'center',
+        top_pad = 0,
+        bottom_pad = 0,
+    },
 })
 
 require("obsidian").setup({
